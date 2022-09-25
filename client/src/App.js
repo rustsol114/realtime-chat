fa8412ae47e3b5322ed0a451bec73c520c8ff9e8
rustsol114 <jamesybarra114@gmail.com>
2022-09-25 12:02:27 +0200

    Migrating to railway.

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,32 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 import { useEffect } from "react"
-import Layout from "./pages/Layout";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import AddFriend from "./pages/AddFriend";
-import ChatBox from "./pages/ChatBox";
-import Requests from "./pages/Requests";
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Protected from "./pages/Protected";
-import { useSelector, useDispatch } from 'react-redux'
-import { getUsers } from './slices/userSlice'
-import { allRequests, newRequest, requestDelete } from './slices/requestSlice'
-import { addConversation, allConversations, rmConversation } from "./slices/conversationSlice";
-import { allRooms, joinRoom, leavedRoom } from "./slices/roomSlice";
-import RoomChat from "./pages/RoomChat";
-import { io } from 'socket.io-client'
-import { setSocket } from "./slices/socketSlice";
-import { brandNewMsg } from "./slices/messageSlice";
+import Layout from "./pages/Layout"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import AddFriend from "./pages/AddFriend"
+import ChatBox from "./pages/ChatBox"
+import Requests from "./pages/Requests"
+import { toast, ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import Protected from "./pages/Protected"
+import { useSelector, useDispatch } from "react-redux"
+import { getUsers } from "./slices/userSlice"
+import { allRequests, newRequest, requestDelete } from "./slices/requestSlice"
+import {
+  addConversation,
+  allConversations,
+  rmConversation,
+} from "./slices/conversationSlice"
+import { allRooms, joinRoom, leavedRoom } from "./slices/roomSlice"
+import RoomChat from "./pages/RoomChat"
+import { io } from "socket.io-client"
+import { setSocket } from "./slices/socketSlice"
+import { brandNewMsg } from "./slices/messageSlice"
 
 function App() {
-  const { user } = useSelector(state => state.auth)
-  const { socket } = useSelector(state => state.socketConfig)
+  const { user } = useSelector((state) => state.auth)
+  const { socket } = useSelector((state) => state.socketConfig)
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -38,7 +38,10 @@ function App() {
     }
 
     // const newSocket = io('http://localhost:3001', { query: { id: user._id } })
-    const newSocket = io('https://socket-chat-mern.herokuapp.com', { query: { id: user._id } })
+    // const newSocket = io('https://socket-chat-mern.herokuapp.com', { query: { id: user._id } })
+    const newSocket = io("https://premium-neck-production.up.railway.app", {
+      query: { id: user._id },
+    })
     dispatch(setSocket(newSocket))
 
     return () => {
@@ -49,70 +52,73 @@ function App() {
   useEffect(() => {
     if (!socket) return
 
-    socket.on('receiveRequest', (userData) => {
+    socket.on("receiveRequest", (userData) => {
       dispatch(newRequest(userData))
-      toast('You have a new friend request', { type: 'info', autoClose: 2000 })
+      toast("You have a new friend request", { type: "info", autoClose: 2000 })
     })
 
-    socket.on('saveConversation', (newConversation) => {
+    socket.on("saveConversation", (newConversation) => {
       dispatch(addConversation(newConversation))
     })
 
-    socket.on('deleteRequest', ({ requestId, reqMsg }) => {
-      toast(reqMsg, { type: 'info', autoClose: 2000 })
+    socket.on("deleteRequest", ({ requestId, reqMsg }) => {
+      toast(reqMsg, { type: "info", autoClose: 2000 })
       dispatch(requestDelete(requestId))
     })
 
-    socket.on('rmConversation', ({ rmMsg, cid }) => {
-      toast(rmMsg, { type: 'error', autoClose: 2000 })
+    socket.on("rmConversation", ({ rmMsg, cid }) => {
+      toast(rmMsg, { type: "error", autoClose: 2000 })
       dispatch(rmConversation(cid))
     })
 
-    socket.on('joinRoom', (room, joinMsg) => {
-      toast(joinMsg, { type: 'info', autoClose: 2000 })
+    socket.on("joinRoom", (room, joinMsg) => {
+      toast(joinMsg, { type: "info", autoClose: 2000 })
       dispatch(joinRoom(room))
     })
 
-    socket.on('leavedRoom', (room, lvMsg, userId) => {
-      toast(lvMsg, { type: 'info', autoClose: 2000 })
+    socket.on("leavedRoom", (room, lvMsg, userId) => {
+      toast(lvMsg, { type: "info", autoClose: 2000 })
       dispatch(leavedRoom({ room, userId }))
     })
 
-    socket.on('brandNewMsg', (newMsg) => {
+    socket.on("brandNewMsg", (newMsg) => {
       dispatch(brandNewMsg(newMsg))
     })
 
     return () => {
-      socket.off('receiveRequest', (userData) => {
+      socket.off("receiveRequest", (userData) => {
         dispatch(newRequest(userData))
-        toast('You have a new friend request', { type: 'info', autoClose: 2000 })
+        toast("You have a new friend request", {
+          type: "info",
+          autoClose: 2000,
+        })
       })
 
-      socket.off('saveConversation', (newConversation) => {
+      socket.off("saveConversation", (newConversation) => {
         dispatch(addConversation(newConversation))
       })
 
-      socket.off('deleteRequest', ({ requestId, reqMsg }) => {
-        toast(reqMsg, { type: 'info', autoClose: 2000 })
+      socket.off("deleteRequest", ({ requestId, reqMsg }) => {
+        toast(reqMsg, { type: "info", autoClose: 2000 })
         dispatch(requestDelete(requestId))
       })
 
-      socket.off('rmConversation', ({ rmMsg, cid }) => {
-        toast(rmMsg, { type: 'error', autoClose: 2000 })
+      socket.off("rmConversation", ({ rmMsg, cid }) => {
+        toast(rmMsg, { type: "error", autoClose: 2000 })
         dispatch(rmConversation(cid))
       })
 
-      socket.off('joinRoom', (room, joinMsg) => {
-        toast(joinMsg, { type: 'info', autoClose: 2000 })
+      socket.off("joinRoom", (room, joinMsg) => {
+        toast(joinMsg, { type: "info", autoClose: 2000 })
         dispatch(joinRoom(room))
       })
 
-      socket.off('leavedRoom', (room, lvMsg, userId) => {
-        toast(lvMsg, { type: 'info', autoClose: 2000 })
+      socket.off("leavedRoom", (room, lvMsg, userId) => {
+        toast(lvMsg, { type: "info", autoClose: 2000 })
         dispatch(leavedRoom({ room, userId }))
       })
 
-      socket.off('newRoomMsg', (newMsg) => {
+      socket.off("newRoomMsg", (newMsg) => {
         dispatch(brandNewMsg(newMsg))
       })
     }
@@ -120,11 +126,8 @@ function App() {
 
   return (
     <div className="App">
-
       <Router>
-
         <Routes>
-
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
 
@@ -164,16 +167,13 @@ function App() {
                 </Protected>
               }
             />
-
           </Route>
-
         </Routes>
-
       </Router>
 
       <ToastContainer position="top-right" newestOnTop />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,15 +1,18 @@
-import axios from 'axios'
+import axios from "axios"
 
 const axiosInstance = axios.create({
-    // baseURL: "http://localhost:5000/api",
-    baseURL: "https://realtime-mern-chat.herokuapp.com/api",
-    withCredentials: true,
+  // baseURL: "http://localhost:5000/api",
+  // baseURL: "https://realtime-mern-chat.herokuapp.com/api",
+  baseURL: "https://realtime-chat-app-production.up.railway.app/api",
+  withCredentials: true,
 })
 
 axiosInstance.interceptors.request.use((req) => {
-    const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null
-    req.headers.Authorization = user ? `Bearer ${user.token}` : ''
-    return req
+  const user = localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : null
+  req.headers.Authorization = user ? `Bearer ${user.token}` : ""
+  return req
 })
 
 export default axiosInstance
